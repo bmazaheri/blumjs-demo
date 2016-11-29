ac5f71ab9d96900e7e49b0cc2ad0ec032e1e000e
Behnam Mazaheri <mazaheri@gmail.com>
2016-11-29 10:20:33 +0330

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
   public someDate: Date = new Date();
+  public ditems: any[];
+
+  ngOnInit() {
+    this.ditems = [{ label: "a", value: { id: 1 } },
+    { label: "b", value: { id: 2 } },
+    { label: "ab", value: { id: 3 } },
+    { label: "aa", value: { id: 4 } },
+    { label: "abc", value: { id: 5 } }
+    ];
+    console.log(this.ditems);
+  }
 }
